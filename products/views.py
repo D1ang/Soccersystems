3cf96dadc2f15afdb89d15a655b4b469a64525d7
Djang <django.heimgartner@gmail.com>
2020-11-23 20:41:01 +0100

    Improved the orders & order items sections.

--- products/views.py
+++ products/views.py
@@ -1,22 +1,40 @@
 from django.shortcuts import render
-from .models import ProductItem
+from .models import ProductGroup, ProductItem
 from .filters import ItemFilter
 
 
-def product_items_list(request):
+def products_list(request):
     """
-    Shows all the available products items
+    Shows all the available product
+    groups and all available product items
     on 1 page.
     """
 
-    item_list = ProductItem.objects.all()
+    product_list = ProductGroup.objects.all()
+    product_item_list = ProductItem.objects.all()
 
-    itemFilter = ItemFilter(request.GET, queryset=item_list)
-    item_list = itemFilter.qs
+    itemFilter = ItemFilter(request.GET, queryset=product_item_list)
+    product_item_list = itemFilter.qs
 
     context = {
-       'item_list': item_list,
-       'itemFilter': itemFilter,
+        'product_list': product_list,
+        'product_item_list': product_item_list,
+        'itemFilter': itemFilter,
     }
 
     return render(request, 'products/items.html', context)
+
+
+def product_items(request, pk_product):
+    """
+    A product items page for the employee
+    to view and order articles.
+    """
+
+    product_item_list = ProductItem.objects.filter(product_group=pk_product)
+
+    context = {
+        'product_item_list': product_item_list
+    }
+
+    return render(request, 'products/product_items.html', context)
