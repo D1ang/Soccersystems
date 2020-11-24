823024e01bca2e5ec6ed045bfc6988d6cbdcb694
Djang <django.heimgartner@gmail.com>
2020-11-24 11:44:13 +0100

    Set the views for supervisor and employee users

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,25 @@
+from .decorators import allowed_users
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+
+@login_required
+@allowed_users(allowed_roles=['supervisor'])
+def adminpage(request):
+    """
+    A view that displays the dashboard
+    for the admin & paginate the order list.
+    """
+
+    return render(request, 'accounts/adminpage.html')
+
+
+@login_required
+@allowed_users(allowed_roles=['employee'])
+def customerpage(request):
+    """
+    A view that displays the dashboard
+    for the customer & paginate the order list.
+    """
+
+    return render(request, 'accounts/customerpage.html')
