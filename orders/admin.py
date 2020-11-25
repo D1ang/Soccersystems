700dc064ba38a17874ad703a2a63040ce71f304d
Djang <django.heimgartner@gmail.com>
2020-11-25 21:00:41 +0100

    Set an admin view for the created models

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import OrderItem, Order
 
-# Register your models here.
+
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = (
+        'item',
+        'user',
+        'ordered'
+    )
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'date',
+        'ordered',
+        'status',
+    )
+
+
+admin.site.register(OrderItem, OrderItemAdmin)
+admin.site.register(Order, OrderAdmin)
