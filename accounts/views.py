bc3b44b2d7b7e339b696fe3f64db9e14b3d3a0ed
Djang <django.heimgartner@gmail.com>
2020-11-28 22:42:56 +0100

    Set an order overview page for the employee users

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,11 @@
 from .decorators import allowed_users
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.core.paginator import Paginator
 
 
 @login_required
-@allowed_users(allowed_roles=['supervisor', 'admin'])
+@allowed_users(allowed_roles=['supervisor'])
 def adminpage(request):
     """
     A view that displays the dashboard
@@ -14,10 +15,25 @@ def adminpage(request):
 
 
 @login_required
-@allowed_users(allowed_roles=['employee'])
+@allowed_users(allowed_roles=['employee', 'admin'])
 def customerpage(request):
     """
     A view that displays the dashboard
-    for the customer & paginate the order list.
+    for the employee & paginate the order list.
     """
-    return render(request, 'accounts/customerpage.html')
+    order_list = request.user.order_set.all().order_by('-date')
+    paginator = Paginator(order_list, 6)
+    page_number = request.GET.get('page')
+    page_object = paginator.get_page(page_number)
+
+    total_orders = order_list.count()
+    pending_orders = order_list.filter(status='pending').count()
+    finished_orders = order_list.filter(status='finished').count()
+
+    context = {
+        'page_object': page_object,
+        'total_orders': total_orders,
+        'pending_orders': pending_orders,
+        'finished_orders': finished_orders,
+    }
+    return render(request, 'accounts/customerpage.html', context)
