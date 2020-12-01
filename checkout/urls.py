f27d99db132c7d7c90de0776ec581bd23af101af
Djang <django.heimgartner@gmail.com>
2020-12-01 08:44:41 +0100

    Removed the old checkout url

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import CheckoutView, PaymentView
+from .views import PaymentView
 
 app_name = 'checkout'
 
 urlpatterns = [
-    path('checkout/', CheckoutView.as_view(), name='checkout'),
     path('payment/', PaymentView.as_view(), name='payment'),
 ]
