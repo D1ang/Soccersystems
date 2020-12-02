550868a32be061ef7e5c85875518ee90d90d308b
Djang <django.heimgartner@gmail.com>
2020-12-02 08:49:06 +0100

    Changed the allowed hosts to localhost

--- soccersystems/settings.py
+++ soccersystems/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['127.0.0.1', 'soccersystems.herokuapp.com']
+ALLOWED_HOSTS = ['soccersystems.herokuapp.com', 'localhost']
 
 
 # Application definition
