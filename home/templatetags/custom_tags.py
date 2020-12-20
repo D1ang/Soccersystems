79d5b725af6a8a8db2f287d29e7b15d6f6a05ae7
Djang <django.heimgartner@gmail.com>
2020-12-20 21:54:12 +0100

    Added comment to custom anchor function

--- home/templatetags/custom_tags.py
+++ home/templatetags/custom_tags.py
@@ -3,6 +3,8 @@ from django.urls import reverse
 
 register = template.Library()
 
+
+# This function creates an anchor link.
 @register.simple_tag
 def anchor(url_name, section_id):
     return reverse(url_name) + '#' + section_id
