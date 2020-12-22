6188fcab9d1d6e359ed950b73c77263b4edc3a6d
Djang <django.heimgartner@gmail.com>
2020-12-22 09:46:03 +0100

    Created the form logic on post

--- home/views.py
+++ home/views.py
@@ -1,8 +1,42 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.template.loader import get_template
+from django.contrib import messages
+
+from django.core.mail import EmailMessage
+from home.forms import ContactForm
 
 
 def index(request):
-    """
-    A view that displays the index page.
-    """
-    return render(request, 'home/index.html')
+    form = ContactForm
+
+    if request.method == 'POST':
+        form = form(data=request.POST)
+
+        if form.is_valid():
+            contact_name = request.POST.get('contact_name', '')
+            contact_email = request.POST.get('contact_email', '')
+            form_content = request.POST.get('content', '')
+
+            # Create the email on the contact template
+            template = get_template('contact_template.txt')
+            context = {
+                'contact_name': contact_name,
+                'contact_email': contact_email,
+                'form_content': form_content
+            }
+            content = template.render(context)
+
+            email = EmailMessage(
+                "Soccersystems bericht",
+                content,
+                "info@ibsgraphics.nl" + '', ['info@ibsgraphics.nl'],
+                headers={'Reply-To': contact_email}
+            )
+            email.send()
+            messages.success(request, 'Message sent successfully')
+            return HttpResponseRedirect('/')
+
+    return render(request, 'home/index.html', {
+        'form': form,
+    })
