0c7c047c64b28a18da3b3f11b679ca260354b549
Djang <django.heimgartner@gmail.com>
2021-03-18 10:49:46 +0100

    Beautified code

--- accounts/decorators.py
+++ accounts/decorators.py
@@ -2,11 +2,11 @@ from django.shortcuts import redirect
 
 
 def allowed_users(allowed_roles=[]):
-    """
+    '''
     Looks for the user group & checks
     if an employee has admin rights to view the admin dashboard.
     If not, the employee will be redirected.
-    """
+    '''
     def decorator(view_func):
         def wrapper_func(request, *args, **kwargs):
 
