31f21991e740ee67769f6cfc4bf06c8aa4824dee
Django Heimgartner <52288307+D1ang@users.noreply.github.com>
2021-12-16 11:01:58 +0100

    Update mail settings to Gmail

--- soccersystems/settings.py
+++ soccersystems/settings.py
@@ -224,7 +224,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 SERVER_EMAIL = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST = 'smtp.office365.com'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
